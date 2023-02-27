503a5bc21d6d9803031263587c10fb9719107481
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-02-28 07:58:03 +0900

    test: add Schedule

---
+++ src/containers/tests/Schedule.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import scheduleReducer from '../../store/reducers/schedule';
+import SchedulePage from '../Schedule';
+
+const rootReducer = combineReducers({
+  schedule: scheduleReducer,
+});
+const store = createStore(rootReducer, applyMiddleware(thunk));
+test('render Schedule Table of PL', async () => {
+  render(
+    <Provider store={store}>
+      <SchedulePage />
+    </Provider>
+  );
+
+  await waitFor(async () => {
+    const tableRows = await screen.findAllByRole('row');
+    expect(tableRows).toHaveLength(13);
+  });
+});
