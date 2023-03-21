1c80e704751de33c12c82425f7f41ed38d7b57bc
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-03-21 21:10:23 +0900

    chore: experimental feature for Safari's browser

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,22 @@
-const { defineConfig } = require("cypress");
-const getCompareSnapshotsPlugin = require("cypress-visual-regression/dist/plugin");
+const { defineConfig } = require('cypress');
+const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin');
 
 module.exports = defineConfig({
-  screenshotsFolder: "./cypress/snapshots/actual",
+  screenshotsFolder: './cypress/snapshots/actual',
   trashAssetsBeforeRuns: true,
   video: false,
-  projectId: "khdjk9",
-
+  projectId: 'khdjk9',
+  experimentalWebKitSupport: true,
   e2e: {
     setupNodeEvents(on, config) {
       getCompareSnapshotsPlugin(on, config);
-    },
+    }
   },
 
   component: {
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
-  },
+      framework: 'create-react-app',
+      bundler: 'webpack'
+    }
+  }
 });
