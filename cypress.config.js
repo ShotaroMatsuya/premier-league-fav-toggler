f3fa37b8c21d1d90e7d3cbcec907f87a144a0eed
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-03-21 20:58:05 +0900

    chore: setup cypress

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+const getCompareSnapshotsPlugin = require("cypress-visual-regression/dist/plugin");
+
+module.exports = defineConfig({
+  screenshotsFolder: "./cypress/snapshots/actual",
+  trashAssetsBeforeRuns: true,
+  video: false,
+  projectId: "khdjk9",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      getCompareSnapshotsPlugin(on, config);
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
