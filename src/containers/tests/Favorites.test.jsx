5a2933f1dba478dc6338bde939e6361d1a2dbcb8
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-03-21 11:30:38 +0900

    test: favorites cmp

--- src/containers/tests/Favorites.test.jsx
+++ src/containers/tests/Favorites.test.jsx
@@ -1,15 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
-import productReducer from '../../store/reducers/players';
+import { mockPlayers } from '../../mocks/mockPlayers';
 import FavoritesPage from '../Favorites';
 
-const rootReducer = combineReducers({
-  player: productReducer
-});
-const store = createStore(rootReducer);
-
 test("should display a message instead of Favorites in Favorites cmp if user don't have favorites", () => {
+  const mockReducer = combineReducers({
+    player: jest.fn().mockReturnValue(mockPlayers)
+  });
+  const store = createStore(mockReducer);
   render(
     <Provider store={store}>
       <FavoritesPage />
@@ -17,4 +16,103 @@ test("should display a message instead of Favorites in Favorites cmp if user don
   );
   const placeholder = screen.getByText(/Got no favorites yet!/i);
   expect(placeholder).toBeInTheDocument();
+});
+
+test('should display one image of player which is marked as favorite', async () => {
+  // mock function
+  const mockState = JSON.parse(JSON.stringify(mockPlayers));
+  mockState.players[0].isFavorite = true;
+  const mockReducer = combineReducers({
+    player: jest.fn().mockReturnValue(mockState)
+  });
+  const mockStore = createStore(mockReducer);
+
+  render(
+    <Provider store={mockStore}>
+      <FavoritesPage />
+    </Provider>
+  );
+  await waitFor(async () => {
+    const favPlayer = screen.getAllByAltText(/fav-player/i);
+    expect(favPlayer).toBeTruthy();
+    expect(favPlayer).toHaveLength(1);
+  });
+});
+
+test('should display two images of players which is marked as favorite twice', async () => {
+  // mock function
+  const mockState = JSON.parse(JSON.stringify(mockPlayers));
+  mockState.players[0].isFavorite = true;
+  mockState.players[1].isFavorite = true;
+  const mockReducer = combineReducers({
+    player: jest.fn().mockReturnValue(mockState)
+  });
+  const mockStore = createStore(mockReducer);
+
+  render(
+    <Provider store={mockStore}>
+      <FavoritesPage />
+    </Provider>
+  );
+  await waitFor(async () => {
+    const favPlayer = screen.getAllByAltText(/fav-player/i);
+    expect(favPlayer).toBeTruthy();
+    expect(favPlayer).toHaveLength(2);
+  });
+});
+
+test('should display correct attributes for field players', async () => {
+  // mock function
+  const mockState = JSON.parse(JSON.stringify(mockPlayers));
+  mockState.players[0].isFavorite = true;
+  const mockReducer = combineReducers({
+    player: jest.fn().mockReturnValue(mockState)
+  });
+  const mockStore = createStore(mockReducer);
+
+  render(
+    <Provider store={mockStore}>
+      <FavoritesPage />
+    </Provider>
+  );
+  await waitFor(async () => {
+    const totalRate = screen.getByText('Rating');
+    expect(totalRate).toBeInTheDocument();
+    const appearanceCount = screen.getByText('出場試合');
+    expect(appearanceCount).toBeInTheDocument();
+    const totalTime = screen.getByText('出場時間');
+    expect(totalTime).toBeInTheDocument();
+    const goalCount = screen.getByText('ゴール数');
+    expect(goalCount).toBeInTheDocument();
+    const assistCount = screen.getByText('アシスト');
+    expect(assistCount).toBeInTheDocument();
+  });
+});
+
+test('should display correct attributes for goal keepers', async () => {
+  // mock function
+  const mockState = JSON.parse(JSON.stringify(mockPlayers));
+  mockState.players[2].isFavorite = true;
+  const mockReducer = combineReducers({
+    player: jest.fn().mockReturnValue(mockState)
+  });
+  const mockStore = createStore(mockReducer);
+
+  render(
+    <Provider store={mockStore}>
+      <FavoritesPage />
+    </Provider>
+  );
+  await waitFor(async () => {
+    const totalRate = screen.getByText('Rating');
+    expect(totalRate).toBeInTheDocument();
+    const appearanceCount = screen.getByText('出場試合');
+    expect(appearanceCount).toBeInTheDocument();
+    const totalTime = screen.getByText('出場時間');
+    expect(totalTime).toBeInTheDocument();
+    const goalCount = screen.getByText('セーブ数');
+    expect(goalCount).toBeInTheDocument();
+    const assistCount = screen.getByText('失点数');
+    expect(assistCount).toBeInTheDocument();
+  });
 });
