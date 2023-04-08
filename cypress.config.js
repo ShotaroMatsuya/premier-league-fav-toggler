d6b0e4a29d5cc28b43c052bed906ac3e197736e0
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-04-08 11:58:01 +0900

    chore: vrt test configuration

--- cypress.config.js
+++ cypress.config.js
@@ -10,9 +10,23 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       getCompareSnapshotsPlugin(on, config);
-    }
+    },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
   },
-
+  viewportWidth: 1280,
+  viewportHeight: 800,
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
+  env: {
+    failSilently: false,
+    SNAPSHOT_BASE_DIRECTORY: 'cypress/snapshots/base',
+    SNAPSHOT_DIFF_DIRECTORY: 'cypress/snapshots/diff',
+    ALWAYS_GENERATE_DIFF: false,
+    type: 'actual'
+  },
   component: {
     devServer: {
       framework: 'create-react-app',
