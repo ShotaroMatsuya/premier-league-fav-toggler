c0a03a2fe76e395b191adb616855340a7eb86a26
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-21 23:52:23 +0900

    commit message

--- src/containers/tests/Schedule.test.jsx
+++ src/containers/tests/Schedule.test.jsx
@@ -34,7 +34,7 @@ test('should have 3 columns in the table', () => {
   expect(tableColumns).toHaveLength(3);
 });
 
-test('should display modal if matches don\'t exist in a week', async() => {
+test("should display modal if matches don't exist in a week", async () => {
   MockDate.set('2023-07-27');
   render(
     <Provider store={store}>
@@ -44,15 +44,15 @@ test('should display modal if matches don\'t exist in a week', async() => {
   await waitFor(async () => {
     const modal = screen.getByTestId('notfound-modal');
     expect(modal).toBeInTheDocument();
-    expect(modal).toHaveTextContent(
-      '1週間以内に試合がありません。'
-    );
+    expect(modal).toHaveTextContent('1週間以内に試合がありません。');
   });
 });
 
-test('should display modal if api call return error', async() => {
+test('should display modal if api call return error', async () => {
   const mockReducer = combineReducers({
-    schedule: jest.fn().mockReturnValue({loading: false, error: true, schedule: null})
+    schedule: jest
+      .fn()
+      .mockReturnValue({ loading: false, error: true, schedule: null })
   });
   const mockStore = createStore(mockReducer, applyMiddleware(thunk));
   render(
@@ -63,8 +63,6 @@ test('should display modal if api call return error', async() => {
   await waitFor(async () => {
     const modal = screen.getByTestId('error-modal');
     expect(modal).toBeInTheDocument();
-    expect(modal).toHaveTextContent(
-      'リクエストに失敗しました'
-    );
+    expect(modal).toHaveTextContent('リクエストに失敗しました');
   });
 });
