1c0e786f2e68d9821c3ca04b27b3f38da8b35f53
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-21 22:31:34 +0900

    tests: add error modal test

--- src/containers/tests/Schedule.test.jsx
+++ src/containers/tests/Schedule.test.jsx
@@ -33,3 +33,38 @@ test('should have 3 columns in the table', () => {
   const tableColumns = screen.getAllByRole('columnheader');
   expect(tableColumns).toHaveLength(3);
 });
+
+test('should display modal if matches don\'t exist in a week', async() => {
+  MockDate.set('2023-07-27');
+  render(
+    <Provider store={store}>
+      <SchedulePage />
+    </Provider>
+  );
+  await waitFor(async () => {
+    const modal = screen.getByTestId('notfound-modal');
+    expect(modal).toBeInTheDocument();
+    expect(modal).toHaveTextContent(
+      '1週間以内に試合がありません。'
+    );
+  });
+});
+
+test('should display modal if api call return error', async() => {
+  const mockReducer = combineReducers({
+    schedule: jest.fn().mockReturnValue({loading: false, error: true, schedule: null})
+  });
+  const mockStore = createStore(mockReducer, applyMiddleware(thunk));
+  render(
+    <Provider store={mockStore}>
+      <SchedulePage />
+    </Provider>
+  );
+  await waitFor(async () => {
+    const modal = screen.getByTestId('error-modal');
+    expect(modal).toBeInTheDocument();
+    expect(modal).toHaveTextContent(
+      'リクエストに失敗しました'
+    );
+  });
+});
